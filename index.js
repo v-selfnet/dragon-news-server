24ed944bb80be5de7c5e3bf7909f42aaf4a6d757
Ashraful Haque <vselfnet@gmail.com>
2023-04-30 15:49:07 +0200

    Serve Update

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors'); // connect between port npm i cors
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors()); // connect between port npm i cors
 
@@ -13,8 +14,30 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectNews = news.find(n => n._id === id);
+    res.send(selectNews);   
+});
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews);
+    }
 })
 
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
-})
+});
